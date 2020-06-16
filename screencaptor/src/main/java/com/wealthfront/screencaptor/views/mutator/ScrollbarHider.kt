8ed0e7a3d4370de6f8a385088de559aa3c70208f
Naren <narendrakumar@wealthfront.com>
2020-06-16 16:24:13 -0700

    Hide scrollbars in list and recycler views as well

--- screencaptor/src/main/java/com/wealthfront/screencaptor/views/mutator/ScrollbarHider.kt
+++ screencaptor/src/main/java/com/wealthfront/screencaptor/views/mutator/ScrollbarHider.kt
@@ -1,15 +1,22 @@
 package com.wealthfront.screencaptor.views.mutator
 
 import android.view.View
+import android.widget.ListView
 import android.widget.ScrollView
 import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Hides the scrollbar from showing up from any type of ScrollView.
  */
 object ScrollbarHider : ViewMutator {
 
-  override fun typesToMutate(): Set<Class<out View>> = setOf(ScrollView::class.java, NestedScrollView::class.java)
+  override fun typesToMutate(): Set<Class<out View>> = setOf(
+      ScrollView::class.java,
+      NestedScrollView::class.java,
+      ListView::class.java,
+      RecyclerView::class.java
+  )
 
   override fun mutateView(listOfViews: List<View>) {
     listOfViews.forEach {
