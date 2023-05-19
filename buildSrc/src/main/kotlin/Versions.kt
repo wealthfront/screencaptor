fc57f3ffed1bad3ecd2440246c34302ea8dec5f3
cmathew <chris.w.mathew@gmail.com>
2023-05-19 16:58:45 -0700

    target 32

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,13 +1,12 @@
 object Versions {
   const val androidLibraryVersion = "7.4.2"
 
-  const val compileSdkVersion = 33
+  const val compileSdkVersion = 32
   const val minSdkVersion = 26
-  const val targetSdkVersion = 33
+  const val targetSdkVersion = 32
 
   const val kotlinVersion = "1.7.21"
-  const val appCompatVersion = "1.5.1"
-  const val recyclerViewVersion = "1.3.0"
+  const val recyclerViewVersion = "1.2.1"
 
   const val robolectricVersion = "4.10.3"
   const val testCoreVersion = "1.5.0"
