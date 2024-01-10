6f203447e07614fee25e739075d854d04d805566
cmathew <chris.w.mathew@gmail.com>
2024-01-10 11:40:42 -0800

    use Java 17

--- screencaptor-test/build.gradle.kts
+++ screencaptor-test/build.gradle.kts
@@ -17,20 +17,8 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
-  compileOptions {
-    sourceCompatibility(JavaVersion.VERSION_1_8)
-    targetCompatibility(JavaVersion.VERSION_1_8)
-  }
   kotlinOptions {
     jvmTarget = "1.8"
-  }
-
-  testOptions.apply {
-    animationsDisabled = true
-    unitTests.apply {
-      isIncludeAndroidResources = true
-      isReturnDefaultValues = true
-    }
   }
 
   namespace = "com.wealthfront.screencaptor.test"
--- screencaptor/build.gradle.kts
+++ screencaptor/build.gradle.kts
@@ -13,20 +13,8 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_1_8)
-        targetCompatibility(JavaVersion.VERSION_1_8)
-    }
     kotlinOptions {
         jvmTarget = "1.8"
-    }
-
-    testOptions.apply {
-        animationsDisabled = true
-        unitTests.apply {
-            isIncludeAndroidResources = true
-            isReturnDefaultValues = true
-        }
     }
 }
 
